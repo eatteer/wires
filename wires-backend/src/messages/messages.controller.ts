fbd1e4098f915013d88591ffa264f3ffef48fe94
Juan Vanegas <eatteer@gmail.com>
2023-03-23 20:10:07 -0500

    Remove log message

--- wires-backend/src/messages/messages.controller.ts
+++ wires-backend/src/messages/messages.controller.ts
@@ -42,7 +42,6 @@ export class MessagesController {
 
   @Post('find')
   findFilterMessages(@Body() findMessage: FindMessageDto): Promise<Message[]> {
-    console.log(findMessage);
     return this.messagesService.findFilterMessage(findMessage);
   }
 
